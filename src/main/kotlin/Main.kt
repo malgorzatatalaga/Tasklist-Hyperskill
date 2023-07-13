111e87e999e54acd8a43df94430e1d8c90d47922
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-07-13 22:29:50 +0200

    menu change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,33 +6,41 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    val listOfTasks = mutableListOf<String>()
+    val listOfTaskClass = mutableListOf<Task>()
 
     while (true) {
         println("Input an action (add, print, edit, delete, end):")
         when (scanner.nextLine().lowercase().trimIndent()) {
-            "add" -> {
-                val priority = inputPriorityAndDateTime()
-                println("Input a new task (enter a blank line to end):")
-                addTask(listOfTasks, priority)
-            }
-
-            "print" -> printListOfTasks(listOfTasks)
-
-
-            "delete" -> deleteTask(listOfTasks)
-
-
             "end" -> {
                 println("Tasklist exiting!")
                 break
             }
 
-            else -> println("The input action is invalid")
+            "add" -> {
+                val priority = inputPriority()
+                val date = inputDate()
+                val time = inputTime()
+                val task = addTask()
 
+            }
+
+            "print" -> {
+
+            }
+
+            "delete" -> {
+
+            }
+
+            "edit" -> {
+
+            }
+
+            else -> {
+                println("The input action is invalid")
+            }
         }
     }
-
 }
 
 class Task (var priority: String, var date: String, var time: String, var dueTag: String, var task: String) {
@@ -61,16 +69,14 @@ fun inputPriority(): String {
     return priority
 }
 
-fun inputDate(): String {
-    var date = ""
-    var validDate = false
-    while (!validDate) {
+fun inputDate(): LocalDate {
+    var date: LocalDate
+    while (true) {
         println("Input the date (yyyy-mm-dd):")
         try {
             val dateList = readln().split("-")
-            val localDate = LocalDate(dateList[0].toInt(), dateList[1].toInt(), dateList[2].toInt())
-            date += localDate.toString().trimIndent()
-            validDate = true
+            date = LocalDate(dateList[0].toInt(), dateList[1].toInt(), dateList[2].toInt())
+            break
         } catch (e: IllegalArgumentException) {
             println("The input date is invalid")
         }
@@ -80,52 +86,40 @@ fun inputDate(): String {
 
 fun inputTime(): String {
     var time = ""
-    while (true) {
+    var validTime = false
+    while (!validTime) {
         println("Input the time (hh:mm):")
         try {
             val timeList = readln().split(":")
-            val localTime = LocalTime(timeList[0].toInt(), timeList[1].toInt())
-            time += "${localTime.hour}:${localTime.minute}"
-            break
-        } catch (e: IllegalArgumentException) {
-            println("The input time is invalid")
-        } catch (e: NumberFormatException) {
+            if (timeList[0].toInt() > 23 || timeList[0].toInt() < 0 || timeList[1].toInt() > 59 || timeList[0].toInt() < 0) {
+                println("The input time is invalid")
+            } else {
+                time += "${timeList[0]}:${timeList[1]}"
+                validTime = true
+            }
+        } catch (e: Exception) {
             println("The input time is invalid")
         }
+
     }
     return time
 }
 
-fun inputPriorityAndDateTime(): String {
-    val priority = inputPriority()
-    val date = inputDate()
-    val time = inputTime()
-
-    return "$date $time $priority"
-}
-
-fun addTask(listOfTasks: MutableList<String>, priorityAndDateTime: String) {
-    var strings = ""
-    val scanner = Scanner(System.`in`)
-    strings += "$priorityAndDateTime$"
-
-    val firstInput = scanner.nextLine().trimIndent()
-    when {
-        firstInput.isBlank() -> {
-            println("The task is blank")
-        }
-
-        else -> {
-            strings += "$firstInput$"
-            do {
-                val multiTask = scanner.nextLine().trimIndent()
-                if (multiTask.isNotEmpty()) {
-                    strings += "$multiTask$"
-                }
-            } while (multiTask.isNotEmpty())
-            listOfTasks.add(strings)
-        }
+fun addTask(): String {
+    println("Input a new task (enter a blank line to end):")
+    var taskInput = ""
+    val firstInput = readln().trimIndent().trimEnd()
+    if (firstInput.isBlank()) {
+        println("The task is blank")
+    } else {
+        taskInput += firstInput
+        do {
+            val singleTask = readln().trimIndent().trimEnd()
+            if (singleTask.isBlank()) continue
+            taskInput += "$$singleTask"
+        } while (singleTask.isNotEmpty())
     }
+    return taskInput
 }
 
 fun printListOfTasks(listOfTasks: MutableList<String>) {
